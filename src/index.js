6ef5ca489f0db00779d53c87225f889b47680d54
Clare Jolly <clajollyuk@gmail.com>
2021-05-05 15:37:14 +0100

    fix: exporting max statements with exclusions rule

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ module.exports = {
   rules: {
     "max-lines-with-context": require("./rules/maxLinesWithContext"),
     "max-lines-per-function-body": require("./rules/maxLintsPerFunctionBody"),
+    "max-statements-with-exclusions": require("./rules/maxStatementsWithExclusions"),
   },
 };
